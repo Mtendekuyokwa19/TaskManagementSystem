3868531f19ba221a00d1f3cad1ec8f894e88f478
MtendeKuyokwa <mtendekuyokwa19@gmail.com>
2024-01-05 06:03:18 +0200

    addition of babel

--- webpack.config.js
+++ webpack.config.js
@@ -37,7 +37,20 @@ module.exports = {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: 'asset/resource',
       },
+      {
+        test: /\.(?:js|mjs|cjs)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['@babel/preset-env', { targets: "defaults" }]
+            ]
+          }
+        }
+      }
     ],
+    
   },
 
  
